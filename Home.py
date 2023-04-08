e68aab5fed2ee26869f6f414db6188f3dece8449
sopida15 <strongnan2315@gmail.com>
2023-04-08 13:50:38 +0700

    .commit

--- Home.py
+++ Home.py
@@ -5,4 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
-st.header("Sopida")
+
+#st.header("Sopida")
+st.image("./pic/npru.png")
+st.image("./pic/dog.jpg")
