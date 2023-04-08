2040600cf9b18bec837afc48d5366c9301e44703
sopida15 <strongnan2315@gmail.com>
2023-04-08 13:54:08 +0700

    .commit

--- Home.py
+++ Home.py
@@ -8,4 +8,12 @@ import plotly.graph_objects as go
 
 #st.header("Sopida")
 st.image("./pic/npru.png")
-st.image("./pic/dog.jpg")
+#st.image("./pic/dog.jpg")
+html_8 = """
+<div style="background-color:#F08080;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+</div>
+"""
+
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
