207ac97fbd641dd7f41523bd3cb2d8c9ca3502a0
sopida15 <strongnan2315@gmail.com>
2023-04-08 14:11:11 +0700

    .commit

--- Home.py
+++ Home.py
@@ -28,9 +28,9 @@ dt4 = dt['sepal.width'].sum()
 
 dx = [dt1, dt2, dt3, dt4]
 dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])\
-    if st.button("แสดงการจินตทัศน์ข้อมูล"):
-       st.write(dt.head(10))
-   st.bar_chart(dx2)
-   st.button("ไม่แสดงข้อมูล")
+if st.button("แสดงการจินตทัศน์ข้อมูล"):
+    #st.write(dt.head(10))
+    st.bar_chart(dx2)
+    st.button("ไม่แสดงข้อมูล")
 else:
     st.write("ไม่แสดงข้อมูล")
