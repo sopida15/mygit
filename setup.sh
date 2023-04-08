613eccc1edb1cf528278d7e1ac81ab4ce6a4e008
sopida15 <strongnan2315@gmail.com>
2023-04-08 13:32:40 +0700

    .commit

---
+++ setup.sh
@@ -0,0 +1,14 @@
+
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"strongnan2315@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
